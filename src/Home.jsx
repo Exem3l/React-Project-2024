8ddba0a7fedfe6eec575cde18c0c39a2b5271adb
Exem3l <119063714+Exem3l@users.noreply.github.com>
2024-07-27 15:02:57 +0400

    Sliders dots design, bug fixes, and small design changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,13 +5,39 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const Home = () => {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-      };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    appendDots: dots => (
+      <div
+        style={{
+          color: "#ff5500",
+          borderRadius: "10px",
+          padding: "10px"
+        }}
+      >
+        <ul style={{ margin: "0",display: "flex", gap:"0px",justifyContent:"center" }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div className='custom-dot'
+        style={{
+          height: "3px",
+          clipPath: "polygon(0% 0%, 85% 0%, 100% 100%, 15% 100%)",
+          color: "#ff5500",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          transition: "all 0.3s ease"
+          
+        }}
+      >
+      </div>
+    )
+  };
   return (
     <div>
       <div className="banner">
@@ -19,11 +45,17 @@ const Home = () => {
         <div className="banner-content">
           <h1>Welcome to AORUS</h1>
           <div className="banner-buttons">
-            <button className="btn white-btn">About</button>
+            <button className="btn orange-btn">About</button>
             <button className="btn gray-btn">News</button>
           </div>
         </div>
       </div>
+      <section className="intro-section">
+        <h2 className="intro-heading">Gaming Devices</h2>
+          <p>
+            AORUS is a leading company in manufacturing high-performance motherboards, graphics cards, laptops, gaming peripherals, and systems. We strive to unite with gamers to boldly push the limits and continue the fight towards ultimate glory!
+          </p>
+      </section>
       <Slider {...settings}>
         <div>
           <h3><img src="src/assets/pc-1.jpg" alt="Slide 1" /></h3>
